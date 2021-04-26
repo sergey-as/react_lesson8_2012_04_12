64405b3616c78c656ba20cf3f4ed917650833810
sergey-as <sergey_as@ukr.net>
2021-04-26 19:09:15 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// https://www.youtube.com/watch?v=naIWIC9L3W0
+// Redux-1
+
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        //
+      </div>
+  );
+}
+
+export default App; 
